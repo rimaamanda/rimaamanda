19d09f2c9b0e787c59047907f9398ff3a8c190ee
rimaamanda <119641180+rimaamanda@users.noreply.github.com>
2022-12-02 09:46:10 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Membuat Template Website Bootstrap</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of 
+         HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+
+    <![endif]-->
+
+    <!-- Custom CSS -->
+    <link href="css/style.css" rel="stylesheet">
+
+  </head>
+  <body>
+    
+    <!--happy code-->
+
+
+    <!-- jQuery online menggunakan CDN -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+
+    
+    <!-- jQuery lokal -->
+    <script src="js/jquery.min.js"></script>
+ 
+    <script src="js/bootstrap.min.js"></script>
+    
+    <!-- Custom JS -->
+    <script src="js/custom.js"></script>
+  </body>
+</html>
+
+<div class="container">
+
+    <!-- Bagian Header -->
+    <div class="row">
+        <div class="col-md-12 header" id="site-header">
+            	<header>
+	    <h1 class="title-site">RIMA MOLLY</h1>
+	    <p class="description">Belajar Membuat Template Website Dengan Bootstrap</p>
+	</header>
+	<nav class="menus">
+	    <ul>
+	        <li><a href="#">Home</a></li>
+	        <li><a href="#">About Us</a></li>
+	        <li><a href="#">Portfolio</a></li>
+	    </ul>
+	</nav>
+
+        </div>
+    </div>
+    <!-- End Bagian Header -->
+
+    <!-- Bagian Wrapper 2 kolom -->
+    <div class="row">
+        <div class="col-md-8 articles" id="site-content">
+           <article class="posts">
+    <h2 class="title-post">Imutnya kucing di indonesia</h2>
+    <div class="meta-post">
+        <span><em class="glyphicon glyphicon-user"></em> Onphpid</span>
+        <span><em class="glyphicon glyphicon-time"></em> 13 Juni 2015</span>
+    </div>
+    <div class="content">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis nisi veniam quibusdam amet, odit, odio consectetur ratione quod, cupiditate repellendus voluptatum. Laudantium tempora, neque quo ex aspernatur veritatis sequi incidunt.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis nisi veniam quibusdam amet, odit, odio consectetur ratione quod, cupiditate repellendus voluptatum. Laudantium tempora, neque quo ex aspernatur veritatis sequi incidunt.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis nisi veniam quibusdam amet, odit, odio consectetur ratione quod, cupiditate repellendus voluptatum. Laudantium tempora, neque quo ex aspernatur veritatis sequi incidunt.</p>
+    </div>
+</article>
+
+        </div>
+        <div class="col-md-4 sidebar" id="site-sidebar">
+            <aside class="widgets">
+    <h3 class="widget-title">Latest Posts</h3>
+    <ul>
+        <li><a href="#">berti besti raka</a></li>
+        <li><a href="#">corla datang ke indonesia</a></li>
+        <li><a href="#">exo-l datang ke lubuklinggau</a></li>
+        <li><a href="#">universitas bina insan</a></li>
+        <li><a href="#">kota lubuklinggau</a></li>
+    </ul>
+</aside>
+
+        </div>
+    </div>
+    <!-- End Bagian wrapper -->
+    
+    <!-- Bagian footer -->
+    <div class="row">
+        <div class="col-md-12 footer" id="site-footer">
+            <footer class=”copyright text-center”><p>&copy; 20222 nisarima.com</p></footer>
+        </div>
+    </div>
+    <!-- End Bagian footer -->
+
+</div>
